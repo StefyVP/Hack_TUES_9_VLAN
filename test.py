7dcd685cee65635fcd3839f4ea9efbb209b55bef
xcWhy <78636566+xcWhy@users.noreply.github.com>
2023-03-11 07:46:08 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-line = "should we use regex more often? let me know at  jdsk@bob.com.lol or popop@coco.com"
-match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', line)
-
-print(match)
-
-line_egn = "0450036377"
-match_egn = re.findall(r'')
